ab53c359c12fdfcfa5c10691f10ce611df74f9df
mjchoi1223 <1971407@hansung.ac.kr>
2024-11-30 19:23:37 +0900

    예산, 통계, 설정 화면 구현하기

---
+++ lib/budgeting_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BudgetingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('설정'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('알림 설정'),
+            onTap: () {
+              // 알림 설정 클릭 시 동작
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('알림 설정 클릭')),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.color_lens),
+            title: Text('테마 변경'),
+            onTap: () {
+              // 테마 변경 클릭 시 동작
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('테마 변경 클릭')),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('앱 정보'),
+            onTap: () {
+              // 앱 정보 클릭 시 동작
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('앱 정보 클릭')),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
